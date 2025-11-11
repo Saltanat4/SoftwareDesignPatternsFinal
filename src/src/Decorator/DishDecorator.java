51e378989235873c9cbf8fea6229cec035dfcb06
zholekeshkyrmyzy <242247@astanait.edu.kz>
2025-11-11 22:12:37 +0500

    decorator and strategy

---
+++ src/src/Decorator/DishDecorator.java
@@ -0,0 +1,18 @@
+package Decorator;
+import FactoryMenu.Dishes.Dish;
+
+public abstract class DishDecorator implements Dish {
+    protected Dish dish;
+
+    public DishDecorator(Dish dish) {
+        this.dish = dish;
+    }
+    @Override
+    public String dishName() {
+        return dish.dishName();
+    }
+    @Override
+    public int getDishPrice(){
+        return dish.getDishPrice();
+    }
+}
