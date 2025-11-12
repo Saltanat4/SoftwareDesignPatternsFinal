42acaef003cfb115a927de22bf420023fb7197a9
Shahnaz-aitu <240843@astanait.edu.kz>
2025-11-13 02:13:13 +0500

    new

--- src/src/FactoryMenu/Dishes/Calculate.java
+++ src/src/FactoryMenu/Dishes/Calculate.java
@@ -1,17 +1,27 @@
 package FactoryMenu.Dishes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Calculate {
     ArrayList<Dish> dishes=new ArrayList<>();
+    private int total = 0;
+
     public void addCalculate(Dish dish) {
         dishes.add(dish);
+        System.out.println();
+        total += dish.getDishPrice();
     }
+
     public int getDish() {
-        int total=0;
-        for (Dish dish: dishes) {
-            total +=  dish.getDishPrice();
-            System.out.println(dish+" " + dish.getDishPrice()+"tg");
-        }return total;
+        return total;
+    }
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+    public List<Dish> getOrderedDishes() {
+        for(Dish dish:dishes) {
+            System.out.println(dish.dishName()+": "+dish.getDishPrice());
+        }return dishes;
     }
 }
